71f76f00a6d14addcd965dd6b70632f574656282
jiyeonpark <jy.park@x-widget.org>
2023-11-06 10:04:06 +0900

    불필요한 코드 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-import Top from "@/app/components/Top";
-import EventModal1 from "@/app/components/eventModal1";
-import SentenceSnow from "@/app/components/sentenceSnow";
-
 export default function Home() {
   return (
         <>
